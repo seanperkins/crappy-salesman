7900a14bbef72ef9cc33b499bb8955563be2e044
Rick Metzger <rickymetz@gmail.com>
2019-06-19 16:02:21 -0400

    Upgrade react scripts, add base styles, add styles and content for role selection

--- src/ui/RoleSelection.js
+++ src/ui/RoleSelection.js
@@ -5,7 +5,26 @@ export default class RoleSelection extends Component {
   render() {
     console.log(this.props)
     return (
-      <div>RoleSelection</div>
+      <React.Fragment>
+        <header>
+          <h1>You're the Customer</h1>
+          <p>Choose your persona for the salespeople to sell to</p>
+        </header>
+        <section className="ui-section">
+          <div className="select-item">
+            <h2>Rick</h2>
+          </div>
+          <div className="select-item is-selected">
+            <h2>Sean</h2>
+          </div>
+          <div className="select-item">
+            <h2>Lin</h2>
+          </div>
+          <div className="select-item">
+            <h2>Chris</h2>
+          </div>
+        </section>
+      </React.Fragment>
     )
   }
 }
